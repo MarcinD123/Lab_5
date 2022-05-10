60d5c7f6c536053ac5bbb54019f226c2c2c98565
MarcinD123 <marcin.dziedzic49@microsoft.wsei.edu.pl>
2022-05-10 22:54:35 +0200

    Add project files.

---
+++ ObservableList1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_5
+{
+    
+
+    class ObservableList1<T>
+    {
+        public event EventHandler<T> NewItemAdded;
+        
+        ListAdded<T> delegat = Program.Cwres<T>;
+
+        //ListAdded LAD = new ListAdded();
+        private List<T> ObList1 = new List<T>();
+        ///private int Length;
+        
+        public int Length
+        {
+            get { return ObList1.Count; }
+
+        }
+        public T this[int i]      //https://docs.microsoft.com/pl-pl/dotnet/csharp/programming-guide/indexers/
+        {
+            get { return ObList1[i]; }
+            set { ObList1[i] = value; }
+        }
+        
+        public void Ob1Add(T item)
+        {
+            
+            ObList1.Add(item);
+            NewItemAdded?.Invoke(this, item);
+            delegat(item);
+        }
+        public void Ob1RemoveAt(int index)
+        { 
+            ObList1.RemoveAt(index);
+        }
+        public T Ob1Get(int index)
+        {
+            return ObList1[index];
+        }
+        public void Ob1Set(T value, int index)
+        {
+            ObList1[index] = value;
+        }
+        // public event EventHandler<string> Addedevent
+
+        
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_5
+{
+    public delegate void ListAdded<T>(T dval);
+    
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            
+            ObservableList1<char> L1 = new Lab_5.ObservableList1<char>();
+            
+            L1.Ob1Add('a');
+            L1.Ob1Add('x');
+            L1.Ob1Add('b');
+            L1.Ob1Add('w');
+            L1.Ob1Add('d');
+            L1.Ob1Set('z', 1);
+            L1.Ob1RemoveAt(2);
+            for (int i = 0; i < L1.Length; i++)
+            {
+                Console.WriteLine(L1.Ob1Get(i));
+            }
+            
+        }
+        static void NewItemAddedHandle(object sender, EventArgs e)
+        {
+            Console.WriteLine($"New value {e} added from{sender.ToString()}");
+        }
+        public static void Cwres<T>(T inp)
+        {
+            Console.WriteLine(inp.ToString());
+        }
+    }
+}
